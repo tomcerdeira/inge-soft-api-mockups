0bacb900e1ba123a8e7b6014c6abd14e7b0f7890
tom <67750968+tomcerdeira@users.noreply.github.com>
2021-06-21 19:02:15 -0300

    proyecto amiwi youtube (anda con postgres)
    https://www.youtube.com/watch?v=vTu2HQrXtyw

--- app/src/test/java/com/example/demo/DemoApplicationTests.java
+++ app/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.unit.app;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AppApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
