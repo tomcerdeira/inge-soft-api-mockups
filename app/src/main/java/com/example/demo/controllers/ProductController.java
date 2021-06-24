b964c383a02e98c982c765a72116d9907a56c744
Santiago <sangarcia@itba.edu,ar>
2021-06-24 17:01:50 -0300

    agrego al path de productos /api_d
    No considero que haya que distinguir los productos por id, por eso solo lo dejo en los par de la funcion y no los uso

--- app/src/main/java/com/example/demo/controllers/ProductController.java
+++ app/src/main/java/com/example/demo/controllers/ProductController.java
@@ -10,28 +10,28 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/product")
+@RequestMapping("/product/{api_id}") // lo agrego aca por consistencia
 public class ProductController {
     @Autowired
     ProductService productService;
 
     @GetMapping()
-    public ArrayList<ProductModel> obtenerProductos(){
+    public ArrayList<ProductModel> obtenerProductos(@PathVariable Integer api_id){
         return productService.obtenerProductos();
     }
 
     @PostMapping()
-    public ProductModel guardarProducto(@RequestBody ProductModel producto){
+    public ProductModel guardarProducto(@RequestBody ProductModel producto,@PathVariable Integer api_id){
         return this.productService.guardarProducto(producto);
     }
 
     @GetMapping( path = "/{id}")
-    public ProductModel obtenerProductoPorId(@PathVariable("id") Long id) {
+    public ProductModel obtenerProductoPorId(@PathVariable("id") Long id,@PathVariable Integer api_id) {
         return this.productService.obtenerPorId(id);
     }
 
     @DeleteMapping( path = "/{id}")
-    public String eliminarPorId(@PathVariable("id") Long id){
+    public String eliminarPorId(@PathVariable("id") Long id,@PathVariable Integer api_id){
         boolean ok = this.productService.eliminarProducto(id);
         if (ok){
             return "Se eliminó el producto con id " + id;
