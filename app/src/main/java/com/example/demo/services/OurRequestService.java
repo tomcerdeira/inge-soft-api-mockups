629a5de0790302cbc137a5b8e11273eb46c4cf80
Santiago <sangarcia@itba.edu,ar>
2021-07-14 17:06:10 -0300

    Update OurRequestService.java

--- app/src/main/java/com/example/demo/services/OurRequestService.java
+++ app/src/main/java/com/example/demo/services/OurRequestService.java
@@ -159,7 +159,7 @@ public class OurRequestService {
                 botDriverProduct.setCapacity(ThreadLocalRandom.current().nextInt(2,4));
                 botDriverProduct.setCancellation_fee(ThreadLocalRandom.current().nextInt(20, 80 + 1));
                 botDriverProduct.setCost_per_minute(ThreadLocalRandom.current().nextInt(2, 12 + 1));
-                botDriverProduct.setCost_per_distance(ThreadLocalRandom.current().nextInt(5, 20 + 1));
+                botDriverProduct.setCost_per_distance(ThreadLocalRandom.current().nextInt(1, 10));
                 botDriverProduct.setMinimum(ThreadLocalRandom.current().nextInt(300, 1000 + 1));
                 botDriverProduct.setCurrency_code("ARS");
                 botDriverProduct.setCash_enabled(true);
